82a84fac45050b3c8b50c6556af2f06293fc2fb2
천현승 <woqkfehlfk@naver.com>
2023-04-21 20:19:00 +0900

    init() Method 추가 및 기본 조회 테스트 코드 commit

--- src/test/java/sample/redis/domain/repository/RedisProductRepositoryTest.java
+++ src/test/java/sample/redis/domain/repository/RedisProductRepositoryTest.java
@@ -1,13 +1,16 @@
 package sample.redis.domain.repository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import sample.redis.domain.application.RedisProductService;
 import sample.redis.domain.domain.Product;
+import sample.redis.domain.dto.ProductResponseDto;
 
 
 @RunWith(SpringRunner.class)
@@ -17,9 +20,16 @@ class RedisProductRepositoryTest {
   @Autowired
   private RedisProductRepository redisProductRepository;
 
+  private RedisProductService redisProductService;
+
+  @BeforeEach
+  public void init() {
+    redisProductService = new RedisProductService(redisProductRepository);
+  }
+
   @Test
   @DisplayName("기본 저장 확인 테스트")
-  void basic_search() {
+  void basic_save() {
 
     //given
     final var id = 0L;
@@ -43,4 +53,29 @@ class RedisProductRepositoryTest {
         .isEqualTo(build.getProductName());
   }
 
+
+  @Test
+  @DisplayName("기본 조회 테스트")
+  void basic_search() {
+    //given
+    final var id = 0L;
+    final var productName = "testName";
+    final var price = 10000L;
+
+    final var build = Product.builder()
+        .id(id)
+        .productName(productName)
+        .price(price)
+        .build();
+
+    //when
+    final var save = redisProductRepository.save(build);
+
+    final var searchRedis = redisProductService.getProduct(save.getId());
+    //then
+    Assertions.assertThat(searchRedis).isNotNull()
+        .isInstanceOf(ProductResponseDto.class)
+        .extracting("productName")
+        .isEqualTo(build.getProductName());
+  }
 }
